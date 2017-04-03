730ecffafd4b0a35f28100cb7324d2bde9cc38f1
Jimmy School <mcdappdev@gmail.com>
2017-04-03 13:40:01 -0500

    upload file

--- Sources/Cloudinary.swift
+++ Sources/Cloudinary.swift
@@ -1,12 +1,13 @@
 import Foundation
 import Vapor
 import HTTP
+import CryptoSwift
 
 final public class Cloudinary {
-    let drop: Droplet
-    
+    private let drop: Droplet
     private let apiKey: String
     private let apiSecret: String
+    private let cloudName: String
     
     init(drop: Droplet) throws {
         self.drop = drop
@@ -19,18 +20,35 @@ final public class Cloudinary {
             throw CloudinaryError.noConfig
         }
         
+        guard let cloudName = drop.config["cloudinary", "cloudName"]?.string else {
+            throw CloudinaryError.noConfig
+        }
+        
         self.apiKey = apiKey
         self.apiSecret = apiSecret
+        self.cloudName = cloudName
     }
     
-    public func upload(file: Data, publicId: String, timestamp: Int? = nil, signature: String? = nil) {
-        
-    }
-    
-    private func createSignature(publicId: String, timestamp: Int? = nil) -> String {
+    public func upload(file: Data, publicId: String, timestamp: Int? = nil, signature: String? = nil) throws {
         let unixTimestamp = timestamp ?? Int(Date().timeIntervalSince1970)
+        let signature = createSignature(publicId: publicId, timestamp: unixTimestamp)
         
-        let signature = "public_id=\(publicId)&timestamp=\(unixTimestamp)\(apiSecret)"
-        return ""
+        let parameters = JSON([
+                "timestamp": try unixTimestamp.makeNode(),
+                "public_id": publicId.makeNode(),
+                "api_key": apiKey.makeNode(),
+                "file": try file.makeNode(),
+                "signature": signature.makeNode()
+        ])
+        
+        return try drop.client.post(url(), headers: [:], query: [:], body: parameters.makeBody())
+    }
+    
+    private func createSignature(publicId: String, timestamp: Int) -> String {
+        return "public_id=\(publicId)&timestamp=\(timestamp)\(apiSecret)".sha1()
+    }
+    
+    private func url() -> String {
+        return "https://api.cloudinary.com/v1_1/\(cloudName)/image/upload"
     }
 }
