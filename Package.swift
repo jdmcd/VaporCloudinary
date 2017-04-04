e9ba42a6505f5692643162daed9fd5906d968119
Jimmy School <mcdappdev@gmail.com>
2017-04-04 13:36:50 -0500

    update package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "VaporCloudinary",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
         .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
     ]
 )
